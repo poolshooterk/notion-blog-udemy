834a3577a64ed12682d5d30d95659adf90b6aee2
gke cicd <cicd.gke.kk@gmail.com>
2023-04-28 10:37:28 +0900

    mod

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -52,20 +52,17 @@ const Post = ({ metadata, markdown }: Props) => {
       <div className="mt-10 font-medium">
         <ReactMarkdown
           components={{
-            code({ node, inline, className, children, ...props }) {
+            code({ node, inline, className, children }) {
               const match = /language-(\w+)/.exec(className || '');
               return !inline && match ? (
                 <SyntaxHighlighter
-                  {...props}
                   children={String(children).replace(/\n$/, '')}
                   style={vscDarkPlus}
                   language={match[1]}
                   PreTag="div"
                 />
               ) : (
-                <code {...props} className={className}>
-                  {children}
-                </code>
+                <code>{children}</code>
               );
             },
           }}
